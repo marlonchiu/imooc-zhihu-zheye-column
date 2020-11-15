33955231568d8ad277a36427c1d8025e02ddc618
marlonchiu <Knight1898@outlook.com>
2020-11-15 08:54:06 +0800

    4-10 Dropdown 组件点击外部区域自动隐藏

--- src/base/Dropdown.vue
+++ src/base/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dropdown">
+  <div class="dropdown" ref="dropdownRef">
     <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">
       {{ title }}
     </a>
@@ -12,7 +12,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
 
 export default defineComponent({
   name: 'Dropdown',
@@ -27,9 +27,28 @@ export default defineComponent({
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+
+    const dropdownRef = ref<null | HTMLElement>(null)
+
+    // 点击事件
+    const handler = (e: MouseEvent) => {
+      if (dropdownRef.value) {
+        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+          isOpen.value = false
+        }
+      }
+    }
+    onMounted(() => {
+      document.addEventListener('click', handler)
+    })
+    onUnmounted(() => {
+      document.addEventListener('click', handler)
+    })
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      // 返回和 ref 同名的响应式对象，就可以拿到对应的 dom 节点
+      dropdownRef
     }
   }
 })
