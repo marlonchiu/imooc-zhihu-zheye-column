203e36d0863f00de18b902cc65f047829e9d18a0
marlonchiu <Knight1898@outlook.com>
2020-11-16 18:49:52 +0800

    6-7 Vuex 简介和安装

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import router from './router'
 import App from './App.vue'
-
+const store = createStore({
+  state: {
+    count: 0
+  },
+  mutations: {
+    add (state) {
+      state.count++
+    }
+  }
+})
+console.log(store.state.count)
+store.commit('add')
+console.log(store.state.count)
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
