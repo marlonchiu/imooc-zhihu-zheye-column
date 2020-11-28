10f29382630391006fcdb9d86a0bc61e187ab364
marlonchiu <Knight1898@outlook.com>
2020-11-28 23:09:47 +0800

    9-2 上传文件的两种实现方式

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="create-post-page">
     <h4>新建文章</h4>
+    <input type="file" name="file" @change.prevent="handleFileChange">
     <validate-form @form-submit="onFormSubmit">
       <div class="mb-3">
         <label class="form-label">文章标题：</label>
@@ -36,6 +37,7 @@ import { GlobalDataProps } from '../store'
 import { PostProps } from '../testData'
 import ValidateInput, { RulesProps } from '../base/ValidateInput.vue'
 import ValidateForm from '../base/ValidateForm.vue'
+import axios from '../libs/http'
 
 export default defineComponent({
   name: 'CreatePost',
@@ -75,12 +77,30 @@ export default defineComponent({
       }
     }
 
+    const handleFileChange = (e: Event) => {
+      const target = e.target as HTMLInputElement
+      const files = target.files
+      if (files) {
+        const uploadedFile = files[0]
+        const formData = new FormData()
+        formData.append(uploadedFile.name, uploadedFile)
+        axios.post('/api/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }).then((resp: any) => {
+          console.log(resp)
+        })
+      }
+    }
+
     return {
       titleVal,
       titleRules,
       contentVal,
       contentRules,
-      onFormSubmit
+      onFormSubmit,
+      handleFileChange
     }
   }
 })
