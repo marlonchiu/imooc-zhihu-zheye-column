0943adf37d55f7b1ffc49dbce577b8de9aef8206
marlonchiu <Knight1898@outlook.com>
2020-12-02 22:08:29 +0800

    11-2 完成帮助函数

--- src/helper.ts
+++ src/helper.ts
@@ -34,3 +34,30 @@ export function beforeUploadCheck (file: File, condition: CheckCondition) {
     error
   }
 }
+
+interface TestProps {
+  _id: string;
+  name: string;
+}
+const testData: TestProps[] = [{ _id: '1', name: 'a' }, { _id: '2', name: 'b' }]
+const testData2: { [key: string]: TestProps } = {
+  1: { _id: '1', name: 'a' },
+  2: { _id: '2', name: 'b' }
+}
+
+export const arrToObj = <T extends {_id?: string}>(arr: Array<T>) => {
+  return arr.reduce((prev, current) => {
+    if (current._id) {
+      prev[current._id] = current
+    }
+    return prev
+  }, {} as { [key: string]: T })
+}
+// const result = arrToObj(testData)
+// console.log(result)
+
+export const objToArr = <T>(obj: {[key: string]: T}) => {
+  return Object.keys(obj).map(key => obj[key])
+}
+// const result2 = objToArr(testData2)
+// console.log(result2)
