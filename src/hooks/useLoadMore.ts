db864ff99e5d10623a2c053374d76f32595af870
marlonchiu <Knight1898@outlook.com>
2020-12-03 09:07:22 +0800

    11-8 useLoadMore 编码

---
+++ src/hooks/useLoadMore.ts
@@ -0,0 +1,32 @@
+import { ref, computed, ComputedRef } from 'vue'
+import { useStore } from 'vuex'
+
+interface LoadParams {
+  currentPage: number;
+  pageSize: number;
+}
+const useLoadMore = (actionName: string, total: ComputedRef<number>, params: LoadParams = { currentPage: 1, pageSize: 5 }) => {
+  const store = useStore()
+  const { currentPage: current, pageSize } = params
+  const currentPage = ref(current)
+  const requestParams = computed(() => ({
+    currentPage: currentPage.value,
+    pageSize: pageSize
+  }))
+  const loadMorePage = () => {
+    store.dispatch(actionName, requestParams.value ).then(() => {
+      currentPage.value++
+    })
+  }
+
+  const isLastPage = computed(() => {
+    return Math.ceil(total.value / pageSize) < currentPage.value
+  })
+  return {
+    loadMorePage,
+    isLastPage,
+    currentPage
+  }
+}
+
+export default useLoadMore
